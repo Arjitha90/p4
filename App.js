0dc2c87b97bca191bc4f0a2c5668b4883ba56851
Arjitha90 <72083952+Arjitha90@users.noreply.github.com>
2021-04-05 07:57:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React,{ Component} from 'react';
+import {StyleSheet , View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+import dictionary from '../database'
+export default class App extends Component{
+  render(){
+    return(
+      <Header />
+    )
+  }
+}
